47c11d3cb59e029fa1126735b6d9b7a98e10392f
Fagner <fagnergeo0@gmail.com>
2025-02-05 11:49:32 -0300

    Installing the community toolkit mvvm nuget package and binding the private text property of the view model with the entry property in xaml using ObservableProperty

---
+++ src/ViewModels/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace src.ViewModels;
+
+class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
---
+++ src/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace src.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    ObservableCollection<string>? items;
+
+    [ObservableProperty]
+    string? text;
+
+    [RelayCommand]
+    void  Add()
+    {
+        //add our item
+        Text = string.Empty;
+    }
+}
